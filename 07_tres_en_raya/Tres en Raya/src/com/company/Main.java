65a36e8a43953fbc1885382b5b115b6690523e4a
Unknown <inaki.caballero@egibide.org>
2016-12-05 14:51:49 +0100

    3 en raya a medias

--- 07_tres_en_raya/Tres en Raya/src/com/company/Main.java
+++ 07_tres_en_raya/Tres en Raya/src/com/company/Main.java
@@ -1,19 +1,34 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void visualizarTablero(int[][] t) {
+    public static void visualizarTablero(int[][] t, int FilaX, int ColumnaY) {
         int fila = t.length;
         int columna = t[0].length;
 
         String tablero[][] = new String[fila][columna];
         for (int i = 0; i < fila; i++) {
             for (int j = 0; j < columna; j++) {
-                tablero[i][j] = ".";
+                if (FilaX == i && ColumnaY == j) {
+                    tablero[i][j] = "X";
+                } else {
+                    tablero[i][j] = ".";
+                }
+                if (t[i][j] == 1) {
+                    tablero[i][j] = "0";
+                } else if (t[i][j] == 2) {
+                    tablero[i][j] = "1";
+                }
 
+                System.out.print("    ");
                 System.out.format(tablero[i][j]);
             }
 
+            System.out.println();
             System.out.println();
         }
     }
@@ -23,10 +38,31 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tablero[][] = new int[3][3];
+        final int turnosPosibles = 9;
+        int fila = -1;
+        int columna = -1;
+        visualizarTablero(tablero, fila, columna);
 
-        visualizarTablero(tablero);
-
+        //Que los jugadores metan la casilla donde ponen la ficha, se representan como 0 y 1 depende del jugador 1 o 2.
+        for (int i = 0; i < turnosPosibles; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Turno de Jugador 1\n" + "Fila(entre 0 y 2): ");
+                fila = Integer.parseInt(br.readLine());
+                System.out.println("Columna(entre 0 y 2): ");
+                columna = Integer.parseInt(br.readLine());
+                visualizarTablero(tablero, fila, columna);
+                tablero[fila][columna] = 1;
+            } else {
+                System.out.println("Turno de Jugador 2\n" + "Fila(entre 0 y 2): ");
+                fila = Integer.parseInt(br.readLine());
+                System.out.println("Columna(entre 0 y 2): ");
+                columna = Integer.parseInt(br.readLine());
+                visualizarTablero(tablero, fila, columna);
+                tablero[fila][columna] = 2;
+            }
+        }
     }
 }
